72cea7c5be1fb9d6e2149e7fdc8ac4fe474acaa8
CVMScris <102757901+CVMScris@users.noreply.github.com>
2022-05-12 14:31:20 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,54 @@
+//1
+
+let numero = prompt('Dos números de 0  a 5. Em qual estou pensando?')
+
+if (numero ==4){
+    console.log('Você acertou. Parabens!!')
+    alert('Você acertou. Parabens!!')
+
+}
+
+else{
+    console.log('Você errou. Que pena ;( ')
+    alert('Você errou. Que pena ;( ')
+}
+
+//2
+
+let vel = prompt('coloque a velocidade do carro')
+if( vel > 80){
+let multa = (vel -80)*5; 
+alert(Você ultrapassou 80km/h, sua multa é de R$ 5,00)
+}
+
+else{
+    alert('Você ultrapassou 80km/h, sua multa é de R$ 5,00');
+}
+
+
+//3
+
+let número = prompt('digite um numero')
+let total n%2 ==0
+if(total == 0){
+    alert('impar')
+}
+
+
+{ alert (total)
+
+}
+
+//4
+
+let salario = prompt('Qual é o valor do seu salario')
+
+if( salario > 1250)
+let aumento = (salario * 10/100) + salario;
+alert(aumento)
+
+else{
+    let aumento2 (salario *15/100)+ salario;
+    alert(aumento2)
+}
+
